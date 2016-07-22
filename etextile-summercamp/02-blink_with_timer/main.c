ded2c38658ec8fff23426ce9454f4bda33c92186
Ingo Randolf <ingo.randolf@servus.at>
2016-07-22 21:39:51 +0200

    add timer ovr example

--- etextile-summercamp/02-blink_with_timer/main.c
+++ etextile-summercamp/02-blink_with_timer/main.c
@@ -11,24 +11,68 @@
 
 // ------- Preamble -------- //
 #include <avr/io.h>                        /* Defines pins, ports, etc */
-#include <util/delay.h>                     /* Functions to waste time */
+#include <avr/interrupt.h>
+
+int speed = 120;
+
+void pinSetup() {
+	//--------------------------------
+	// pin setup
+	// -------- Inits --------- //
+	DDRB = 0b00000001;            // Data Direction Register B: writing a one to the bit
+	// enables output.
+}
+
+void timer0Setup() {
+	//--------------------------------
+	// CTC mode - counting direction up
+	TCCR0B &= ~(1 << WGM02);
+	TCCR0A &= ~(1 << WGM00);
+	TCCR0A &= ~(1 << WGM01);
+	
+	
+	//--------------------------------
+	// set prescaler
+	TCCR0B |= (1 << CS02) | (1 << CS00); // clk_io/1024
+	
+	//--------------------------------
+	// preload timer counter
+	TCNT0 = speed;
+	
+	//--------------------------------
+	// enable overflow interrupt
+	TIMSK |= (1 << TOIE0);
+}
+
+// timer0 overflow interrupt
+ISR(TIMER0_OVF_vect) {
+
+	// check if pin is high
+	if( (PORTB & (1 << PB0)) == 0 ) {
+		// pin is off - turn it on
+		PORTB |= (1 << PB0);
+	} else {
+		// pin is on - turn it off
+		PORTB &= ~(1 << PB0);
+	}
+	
+	TCNT0 = speed;
+}
 
 
 int main(void) {
 
-  // -------- Inits --------- //
-  DDRB = 0b0000001;            // Data Direction Register B: writing a one to the bit
-                               // enables output.
-
-  // ------ Event loop ------ //
-  while (1) {
-
-    PORTB = 0b00000001;          // Turn on first LED bit/pin in PORTB 
-    _delay_ms(1000);             // wait
-
-    PORTB = 0b00000000;          // Turn off all B pins, including LED
-    _delay_ms(1000);             // wait 
-
-  }                              // End event loop 
-  return 0;                      // This line is never reached
+	pinSetup();
+	
+	timer0Setup();
+	
+	sei();
+	
+	// ------ Event loop ------ //
+	while (1) {
+		// idle
+	}
+	
+	// This line is never reached
+	return 0;
 }
